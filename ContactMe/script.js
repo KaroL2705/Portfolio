a105bd8f7752a0d49ee875d985ef76e50e8b6fa4
Karol Swiercz <swierczkarol27@gmail.com>
2024-01-31 23:41:25 +0100

    fix paths

--- ContactMe/script.js
+++
@@ -1,3 +0,0 @@
-let submitMessage = () => {
-    alert("Message was send");
-}
---
+++ script.js
@@ -0,0 +1,28 @@
+let calculateCost = () => {
+    var wage = document.getElementById("hourlyWage").value;
+    var hours = document.getElementById("HoursWorked").value;
+    var total = wage * hours;
+
+    document.getElementById('totalCost').textContent = total.toFixed(2);
+}
+
+let insertProposedValues = () => {
+    var selectedOption = document.getElementById("SelectType").value;
+    var hourlyWage = document.getElementById("hourlyWage");
+    var hoursWorked = document.getElementById("HoursWorked");
+
+    if (selectedOption == 1){
+        hourlyWage.value = 40;
+        hoursWorked.value = 40;
+    } else if (selectedOption == 2){
+        hourlyWage.value = 30;
+        hoursWorked.value = 10;
+    } else if (selectedOption == 3){
+        hourlyWage.value = 50;
+        hoursWorked.value = 160;
+    }
+}
+
+let submitMessage = () => {
+    alert("Message was send");
+}
